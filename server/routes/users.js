7fd798cf6041061e4d72e3c34c7e30c636ebdafa
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-01-07 20:23:55 +0900

    Node.js MongoDB JWT Cookie Authentication

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,20 @@
 import express from "express";
-import { } from "../controllers/user.js"
+import { update, deleteUser, getUser, subscribe, unsubscribe, like, dislike,  } from "../controllers/user.js"
 
 const router = express.Router();
+
+router.put("/:id", update)
+
+router.delete("/:id", deleteUser)
+
+router.get("/find/:id", getUser)
+
+router.put("/sub/:id", subscribe)
+
+router.put("/ubsub/:id", unsubscribe)
+
+router.put("/like/:videoId", like)
+
+router.put("/unlike/:videoId", dislike)
 
 export default router;
