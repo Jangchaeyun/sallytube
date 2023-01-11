db5b8be6c0c26fd117c2460a740f55b5b5518e3a
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-01-11 22:55:18 +0900

    React Google Auth with MongoDB

--- server/models/User.js
+++ server/models/User.js
@@ -13,8 +13,6 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-        unique: true
     },
     img: {
         type: String
@@ -26,6 +24,10 @@ const UserSchema = new mongoose.Schema({
     subscribedUsers: {
         type: [String]
     },
+    fromGoogle: {
+        type: Boolean,
+        default: false,
+    }
 }, {timestamps: true});
 
 export default mongoose.model("User", UserSchema)
